05833c7ef55001a762fccc518928c11b68cfa546
Satyajeet Singh <satyajeet1152000@gmail.com>
2025-01-19 19:58:37 +0530

    last update2

--- client/app/(private)/layout.tsx
+++ client/app/(private)/layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/Navbar/Navbar";
 import { ModalProvider } from "@/context/ModelContext";
-import { ThemeProvider } from "@/components/ThemeProvider";
+// import { ThemeProvider } from "@/components/ThemeProvider";
 import React from "react";
 
 const layout = ({
@@ -10,25 +10,18 @@ const layout = ({
 }>) => {
 	return (
 		<main>
-			<ThemeProvider
-				attribute='class'
-				defaultTheme='system'
-				enableSystem
-				disableTransitionOnChange
-			>
-				{/* <AuthProvider> */}
-				<ModalProvider>
-					<div className='flex'>
-						<div className='w-72 flex-none relative'>
-							<div className='fixed w-72 h-full border-r-2 border-gray-200 dark:border-gray-500 rounded-r-xl'>
-								<Navbar />
-							</div>
+			{/* <AuthProvider> */}
+			<ModalProvider>
+				<div className='flex'>
+					<div className='w-72 flex-none relative'>
+						<div className='fixed w-72 h-full border-r-2 border-gray-200 dark:border-gray-500 rounded-r-xl'>
+							<Navbar />
 						</div>
-						<div className=' flex-grow m-5 '>{children}</div>
 					</div>
-				</ModalProvider>
-				{/* </AuthProvider> */}
-			</ThemeProvider>
+					<div className=' flex-grow m-5 '>{children}</div>
+				</div>
+			</ModalProvider>
+			{/* </AuthProvider> */}
 		</main>
 	);
 };
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@/context/UserContext";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -28,7 +29,14 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<UserProvider>{children}</UserProvider>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange
+				>
+					<UserProvider>{children}</UserProvider>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
