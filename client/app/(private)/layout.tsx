f3c24dc3be60294cf063b0d58aab378d619a7177
Satyajeet Singh <satyajeet1152000@gmail.com>
2025-01-19 20:05:33 +0530

    last update3

--- client/app/(private)/layout.tsx
+++ client/app/(private)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar/Navbar";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import { ModalProvider } from "@/context/ModelContext";
 // import { ThemeProvider } from "@/components/ThemeProvider";
 import React from "react";
@@ -9,20 +10,27 @@ const layout = ({
 	children: React.ReactNode;
 }>) => {
 	return (
-		<main>
-			{/* <AuthProvider> */}
-			<ModalProvider>
-				<div className='flex'>
-					<div className='w-72 flex-none relative'>
-						<div className='fixed w-72 h-full border-r-2 border-gray-200 dark:border-gray-500 rounded-r-xl'>
-							<Navbar />
+		<ThemeProvider
+			attribute='class'
+			defaultTheme='system'
+			enableSystem
+			disableTransitionOnChange
+		>
+			<main>
+				{/* <AuthProvider> */}
+				<ModalProvider>
+					<div className='flex bg-[#020612]'>
+						<div className='w-72 flex-none relative'>
+							<div className='fixed w-72 h-full border-r-2 border-gray-200 dark:border-gray-500 rounded-r-xl'>
+								<Navbar />
+							</div>
 						</div>
+						<div className=' flex-grow m-5 '>{children}</div>
 					</div>
-					<div className=' flex-grow m-5 '>{children}</div>
-				</div>
-			</ModalProvider>
-			{/* </AuthProvider> */}
-		</main>
+				</ModalProvider>
+				{/* </AuthProvider> */}
+			</main>
+		</ThemeProvider>
 	);
 };
 
